279df8d629d6340317005c94ba989e74b7cd1f8a
VictorAndreiCotescu <victor.cotescu@yahoo.com>
2021-06-08 16:59:24 +0300

    working is_pdf function (ex2)

--- CRVopps/R/norm_const.R
+++ CRVopps/R/norm_const.R
@@ -11,6 +11,8 @@ constantaDeNormalizare <- function(){
 
     eval(parse(text = paste('f <- function(', 'x', ') { return(' , my.func , ')}', sep='')))
 
+    #return (ifelse( ((exp(1))^(( -(x^2)/2 ))) == Inf | ((exp(1))^(( -(x^2)/2 ))) == -Inf, 0, ((exp(1))^(( -(x^2)/2 ))) ))
+
     return (ifelse( f(x) == Inf | f(x) == -Inf, 0, f(x)))
 
   }
@@ -20,6 +22,10 @@ constantaDeNormalizare <- function(){
   rez  <-  cubintegrate(resolve1, lower = -Inf, upper = Inf, method = 'pcubature');
   }
 
+  #integr <- function(x) { ((exp(1))^(( -(x^2)/2 )))*(1/rez$integral)}
+
+  #rez2 = cubintegrate(integr, lower = -Inf, upper = Inf, method = 'pcubature');
+  #eval(rez2$integral)
 
   ifelse ( is.na(rez$integral) | rez$integral == Inf | rez$integral == -Inf, "Functia introdusa nu are constanta de normalizare", 1/rez$integral )
 
