12eb64284fd6a281122060cc735454ef72291798
VictorAndreiCotescu <victor.cotescu@yahoo.com>
2021-06-09 15:37:56 +0300

    formatare

--- CRVopps/R/is_pdf.r
+++ CRVopps/R/is_pdf.r
@@ -10,18 +10,12 @@ densitateDeProbabilitate <- function(){
 
     eval(parse(text = paste('f <- function(', 'x', ') { return(' , my.func , ')}', sep='')))
 
-    #return (ifelse( ((exp(1))^(( -(x^2)/2 ))) == Inf | ((exp(1))^(( -(x^2)/2 ))) == -Inf, 0, ((exp(1))^(( -(x^2)/2 ))) ))
-
-    return (ifelse( f(x) == Inf | f(x) == -Inf, 0, 0.3989423*f(x)))
+    return (ifelse( f(x) == Inf | f(x) == -Inf, 0, f(x)))
 
   }
 
 
-  # exp(1)^(-((x^2)/2))
-  # rel.tol = 1e-15 eroare relativa
   rez <- cubintegrate(resolve1, lower = -Inf, upper = Inf, method = "pcubature")
-  rez2 <- integrate(resolve1, lower = -Inf, upper = Inf)
-
 
   ifelse( floor(rez$integral) == 1, return("Functia este densitate de probabilitate"), return ("Functia nu este densitate de probabilitate"))
 
--- CRVopps/R/norm_const.R
+++ CRVopps/R/norm_const.R
@@ -4,28 +4,19 @@ package(cubature)
 constantaDeNormalizare <- function(){
 
   {
-  my.func <- readline(prompt="Introduceti functia: ")
+    my.func <- readline(prompt="Introduceti functia: ")
 
 
-  resolve1 <-  function(x){
+    resolve1 <-  function(x){
 
-    eval(parse(text = paste('f <- function(', 'x', ') { return(' , my.func , ')}', sep='')))
+      eval(parse(text = paste('f <- function(', 'x', ') { return(' , my.func , ')}', sep='')))
 
-    #return (ifelse( ((exp(1))^(( -(x^2)/2 ))) == Inf | ((exp(1))^(( -(x^2)/2 ))) == -Inf, 0, ((exp(1))^(( -(x^2)/2 ))) ))
+      return (ifelse( f(x) == Inf | f(x) == -Inf, 0, f(x)))
 
-    return (ifelse( f(x) == Inf | f(x) == -Inf, 0, f(x)))
+    }
 
+    rez  <-  cubintegrate(resolve1, lower = -Inf, upper = Inf, method = 'pcubature');
   }
-
-
-
-  rez  <-  cubintegrate(resolve1, lower = -Inf, upper = Inf, method = 'pcubature');
-  }
-
-  #integr <- function(x) { ((exp(1))^(( -(x^2)/2 )))*(1/rez$integral)}
-
-  #rez2 = cubintegrate(integr, lower = -Inf, upper = Inf, method = 'pcubature');
-  #eval(rez2$integral)
 
   ifelse ( is.na(rez$integral) | rez$integral == Inf | rez$integral == -Inf, "Functia introdusa nu are constanta de normalizare", 1/rez$integral )
 
