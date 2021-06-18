2a0dcf8698d87c24d2a956cbc1c43f4265674e92
VictorAndreiCotescu <victor.cotescu@yahoo.com>
2021-06-18 14:03:50 +0300

    fin

--- CRVopps/R/12_sum_diff.R
+++ CRVopps/R/12_sum_diff.R
@@ -1,23 +1,29 @@
 library(pracma)
 
-sum <- function(crv1, crv2){
-
-  low <- as.integer(readline(prompt="Define the sum domain:\nLow: \n"))
-  high <- as.integer(readline(prompt="High: "))
-
-  new_func <- function(z, y){}
-  crv1_func_retext <- gsub("x", "(z-y)", crv1[1])
-  crv2_func_retext <- gsub("x", "y", crv2[1])
-
-  new_func_body <-  paste(crv1_func_retext, crv2_func_retext, sep="*")
-
-  body(new_func) <- parse(text = new_func_body)
-
-  return(integral2(new_func, low, high, low, high))
-
+convolution_sum <- function(fx,fy) {
+  function(z) {
+    integrate(function(y) {
+      fx(z-y) * fy(y)
+    },-Inf,Inf)
+  } $ value
 }
 
-crv1 <- c("x+1", 0, 10)
-crv2 <- c("x-1", 0, 10)
 
-sum(crv1, crv2)
+convolution_diff <- function(fx,fy) {
+  function(z) {
+    integrate(function(y) {
+      fx(y-z)*fy(y)
+    },-Inf,Inf)
+  } $ value
+}
+
+
+f <- function(x)(dnorm(x,1)) #distributie normala cu mean = 1
+g <- function(x) (dnorm(x,2))
+sum <- Vectorize(convolution_sum(f, g))
+sub <- Vectorize(convolution_diff(f, g))
+
+
+par(mfrow=c(0,2))
+plot(sum,from=-5,to=6,type="l")
+plot(sub,from=-5,to=6,type="l")
